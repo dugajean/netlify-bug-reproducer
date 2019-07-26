d86dba7fcd2f4e251f0bd044da2ab6610098d19b
Dugi <me@dukagj.in>
2019-07-26 21:35:28 +0200

    Initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <form name="myform" data-netlify="true" @submit.prevent="handleSubmit">
+      <input type="text" name="name" v-model="name">
+      <input type="text" name="email" v-model="email">
+      <button type="submit">Submit</button>
+      <div v-if="sent">Sent!</div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      sent: false
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const formData = `form-name=myform&name=${this.name}&email=${this.email}`
+
+      const axiosConfig = {
+          header: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      }
+
+      axios.post(event.target.action, formData, axiosConfig)
+          .then(() => { this.sent = true })
+          .catch(() => { this.sent = false })
+    }
+  }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <nuxt />
+    <Logo />
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: { Logo }
+}
+</script>
